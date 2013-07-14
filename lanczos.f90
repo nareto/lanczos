ebc99ea0f159f37f009c72323ebdcbf454d2dffd
jboogie <rennabh@gmail.com>
2013-07-14 23:09:20 +0200

    subroutine for entering tridiagonal matrix from diagonal vectors and another to neat-print matrixes

--- lanczos.f90
+++ lanczos.f90
@@ -1,20 +1,68 @@
 PROGRAM lanczos
   IMPLICIT NONE
+  INTEGER, PARAMETER :: dp=KIND(0.d0), ndiprova = 5
+  INTEGER :: i
+  REAL(dp), DIMENSION(ndiprova, ndiprova) ::  T
   INTEGER, PARAMETER :: dim = 500
   !INTEGER, DIMENSION(N,N) :: A
-  INTEGER, DIMENSION(dim) :: v
-  INTEGER :: i
+  REAL(dp), DIMENSION(ndiprova) :: v
+  REAL(dp), DIMENSION(ndiprova - 1) :: w
 
-  
-  !v = (/ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 /)
-  !do i = 1, dim
-  !   v(i) = i
+  !do i = 1, ndiprova - 1
+  !   v(i) = real(i)
+  !   w(i) = real(5)
   !end do
-  !CALL prodotto(v, dim)
-  
+  !v(ndiprova) = ndiprova
+  !
+  !call tridiag(T, v, w, ndiprova)
+  !call m_print(T)
+
   call lanczos_naive(dim)
 
   CONTAINS
+
+    subroutine m_print(A)
+      INTEGER, PARAMETER :: dp=KIND(0.d0)
+      real(dp), dimension(:,:), intent(in) :: A
+      integer :: m, n, i, j
+      m=size(A,dim=1)
+      n=size(A,dim=2)
+      do i=1,m
+         print "(9f10.4)", (A(i,j),j=1,n)
+      end do
+      print *
+    end subroutine m_print
+
+  SUBROUTINE tridiag(T, D, U, dim) !creates symmetric tridiagonal matrix (stores in T) from vectors D (diagonal) and U (upper diagonal) of dimension dim and dim-1
+    IMPLICIT NONE
+    INTEGER, PARAMETER :: dp=KIND(0.d0)
+    INTEGER :: i, j, dim
+    REAL(dp), DIMENSION(dim,dim) :: T
+    REAL(dp), DIMENSION(dim) :: D
+    REAL(dp), DIMENSION(dim - 1) :: U
+
+    T(1,1) = 1.0
+    T(1,2) = 5.0
+    do j=3, dim
+       T(1,j) = 0
+    end do
+    do i =2, dim-1 !righe
+       do j=1, i-2
+          T(i,j) = 0
+       end do
+       T(i,i-1) = U(i-1) 
+       T(i,i) = D(i)
+       T(i,i+1) = U(i)
+       do j=i+2, dim
+          T(i,j) = 0
+       end do
+    end do
+    do j=1, dim-2
+       T(dim,j) = 0
+    end do
+    T(dim,dim-1) = U(dim-1)
+    T(dim,dim) = D(dim)
+  END SUBROUTINE tridiag
 
   SUBROUTINE prodotto(invec, dim, outvec)
     !calcola outvec=A invec
@@ -39,9 +87,9 @@ PROGRAM lanczos
   END SUBROUTINE prodotto
 
   SUBROUTINE lanczos_naive(dim)
-    INTEGER :: i, dim
+    INTEGER :: i, j, dim
     INTEGER, PARAMETER :: dp=KIND(0.d0)
-    REAL(dp), DIMENSION(dim,dim) :: Q
+    REAL(dp), DIMENSION(dim,dim) :: Q, T
     REAL(dp), DIMENSION(dim) :: rnd, tmp, alfa, beta, r
     character :: JOBZ = 'N', RANGE = 'A', UPLO = 'U'
 
@@ -67,11 +115,11 @@ PROGRAM lanczos
           Q(:,i+1) = r/beta(i)
        end if
     end do
-
     
 
-    !call dsyevr( JOBZ, RANGE, UPLO, dim, A, LDA, VL, VU, IL, IU, ABSTOL, M, W, Z, LDZ, ISUPPZ, WORK, LWORK, IWORK, LIWORK, INFO )
+    !call dsyevr( JOBZ, RANGE, UPLO, dim, T, dim, VL, VU, IL, IU, ABSTOL, M, W, Z, LDZ, ISUPPZ, WORK, LWORK, IWORK, LIWORK, INFO )
        
   END SUBROUTINE lanczos_naive
+
 
 END PROGRAM lanczos
